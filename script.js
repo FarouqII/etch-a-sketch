3c49bd1230d9fca9c5b95fec1e1aa23d940d5202
FarouqII <farouq006@gmail.com>
2025-04-03 22:08:48 +0300

    Add clear buttton
    Added the clear button and its functionality

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const container = document.querySelector("#grid");
+const clear = document.querySelector("#clear");
+const rainbow = document.querySelector("#rainbow");
 
 for (i = 0; i < 16; i++) {
     let row = document.createElement("div");
     row.id = "grid-row";
     grid.appendChild(row);
-    console.log("New row created")
 
     for (j = 0; j < 16; j++) {
         let tile = document.createElement("div");
         tile.style.width = `${640 / 16}px`;
         tile.style.height = `${640 / 16}px`;
-        tile.id = "grid-tile";
+        tile.className = "grid-tile";
         tile.addEventListener("mouseover", () => {
-            tile.style.background = "#3A3B3C";
-            tile.style.border = "1px solid white";
+            tile.style.background = "#6F6F6F";
+            tile.style.border = "1px solid #6F6F6F";
         })
         row.appendChild(tile);
     }
 }
+
+clear.addEventListener("click", () => {
+    const tiles = document.querySelectorAll(".grid-tile");
+    tiles.forEach(tile => {
+        tile.style.border = "1px solid gray";
+        tile.style.background = "white";
+    })
+})
